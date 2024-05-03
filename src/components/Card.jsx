e0f585a946559e67388694e58c4628e30ea19c2d
Noxolo <wendyngcobo98@gmail.com>
2024-05-03 12:26:08 +0200

    Update card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
-const Card = (children) => {
-    return (<>
-    </>);
+const Card = ({children,bg = "bg-gray-100"}) => {
+    
+    return (<div className={`${bg} p-6 rounded-lg shadow-md`}>
+        {children}
+    </div>);
 
 };
 export default Card;
